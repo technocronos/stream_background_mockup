d9e6d9a66590f64aac1acc25f60bf554421191be
Alexander Zhukov <alexulter@gmail.com>
2025-10-17 12:24:31 +0300

    3 scenes

---
+++ Stream Background/Assets/Scripts/SimpleSceneLoader.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SimpleSceneLoader : MonoBehaviour
+{
+    public void LoadSceneEffects()
+    {
+        SceneManager.LoadScene("PlayableEffects");
+    }
+
+    public void LoadSceneVoting()
+    {
+        SceneManager.LoadScene("Voting");
+    }
+
+    public void LoadSceneProgress()
+    {
+        SceneManager.LoadScene("ProgressGauge");
+    }
+
+    public void LoadSceneSelection()
+    {
+        SceneManager.LoadScene("_Start_");
+    }
+}
